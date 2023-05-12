2b9aa5faebf4e52337668547a67088fb97bb3c20
Coding With Enjoy <codingwithenjoy@gmail.com>
2023-05-12 14:41:53 +0330

    Create index.jsx

---
+++ src/components/Home/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+const Footer = () => (
+  <footer className="home-footer">
+    <h2>دنیای نجوم</h2>
+    <h1>
+      <span>“</span> علامه حلی 6 <span>”</span>
+    </h1>
+    <p>
+      تمامی محتوای این سایت محفوظ می باشد
+    </p>
+  </footer>
+);
+
+export default Footer;
